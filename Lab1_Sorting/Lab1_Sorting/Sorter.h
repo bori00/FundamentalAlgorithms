462e28b3ace59d3fb7aed510394e148b1b8183b7
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-04 00:41:02 +0300

    setup

---
+++ Lab1_Sorting/Lab1_Sorting/Sorter.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class Sorter
+{
+public:
+	
+};
---
+++ Lab1_Sorting/Lab1_Sorting/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 2;
+	cout << a << endl;
+	// todo: here
+	return 0;
+}
