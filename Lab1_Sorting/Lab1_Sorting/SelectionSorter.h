8909604504791e5fc224502b71ecd0e4565e04c9
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-04 12:30:44 +0300

    remove visual studio project

--- Lab1_Sorting/Lab1_Sorting/SelectionSorter.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "Sorter.h"
-
-class SelectionSorter: public Sorter
-{
-public:
-	void sort(int* a, int no_elements) override;
-};
--- Lab1_Sorting/Lab1_Sorting/Sorter.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-class Sorter
-{
-public:
-	
-};
