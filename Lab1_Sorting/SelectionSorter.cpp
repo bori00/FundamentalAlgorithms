5aef68d8f043c2c20efa4302a8f51455a18b7461
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-09 13:30:04 +0300

    reduce no iterations in selection sort

--- Lab1_Sorting/SelectionSorter.cpp
+++ Lab1_Sorting/SelectionSorter.cpp
@@ -12,7 +12,7 @@ const char *SelectionSorter::SORTER_NAME = "SelectionSort";
 void SelectionSorter::sort(int *v, int no_elements, Profiler &p) {
   Operation opComp = p.createOperation(COMP_OP_NAME, no_elements);
   Operation opAssign = p.createOperation(ASSIGN_OP_NAME, no_elements);
-  for (int i = 0; i < no_elements; i++) {
+  for (int i = 0; i < no_elements - 1; i++) {
     int minIndex = i;
     for (int j = i + 1; j < no_elements; j++) {
       opComp.count();
