f6869e3a88ff651de87ad24ec44c1965051b2d7f
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-10 15:38:17 +0300

    add tests for top-down heap building

--- Lab1_Sorting/heap_tests.cc
+++ Lab1_Sorting/heap_tests.cc
@@ -13,38 +13,93 @@ static Profiler ignorePHeap("ignore");
 static Operation op_comp = ignorePHeap.createOperation("op_comp", 0);
 static Operation op_assign = ignorePHeap.createOperation("op_assign", 0);
 
-TEST_F(HeapTest, MaxHeap_AverageArray) {
+TEST_F(HeapTest, BuildHeap_BottomUp_MaxHeap_AverageArray) {
   int *v = GetAverageArray();
   Heap h(v, kArrayLength, Heap::HeapType::kMaxHeap, &op_comp, &op_assign);
   EXPECT_TRUE(HeapTest::isHeap(h));
 }
 
-TEST_F(HeapTest, MaxHeap_SortedArray) {
+TEST_F(HeapTest, BuildHeap_BottomUp_MaxHeap_SortedArray) {
   int *v = GetSortedArray();
   Heap h(v, kArrayLength, Heap::HeapType::kMaxHeap, &op_comp, &op_assign);
   EXPECT_TRUE(HeapTest::isHeap(h));
 }
 
-TEST_F(HeapTest, MaxHeap_InvSortedArray) {
+TEST_F(HeapTest, BuildHeap_BottomUp_MaxHeap_InvSortedArray) {
   int *v = GetInverselySortedArray();
   Heap h(v, kArrayLength, Heap::HeapType::kMaxHeap, &op_comp, &op_assign);
   EXPECT_TRUE(HeapTest::isHeap(h));
 }
 
-TEST_F(HeapTest, MinHeap_AverageArray) {
+TEST_F(HeapTest, BuildHeap_BottomUp_MinHeap_AverageArray) {
   int *v = GetAverageArray();
   Heap h(v, kArrayLength, Heap::HeapType::kMinHeap, &op_comp, &op_assign);
   EXPECT_TRUE(HeapTest::isHeap(h));
 }
 
-TEST_F(HeapTest, MinHeap_SortedArray) {
+TEST_F(HeapTest, BuildHeap_BottomUp_MinHeap_SortedArray) {
   int *v = GetSortedArray();
   Heap h(v, kArrayLength, Heap::HeapType::kMinHeap, &op_comp, &op_assign);
   EXPECT_TRUE(HeapTest::isHeap(h));
 }
 
-TEST_F(HeapTest, MinHeap_InvSortedArray) {
+TEST_F(HeapTest, BuildHeap_BottomUp_MinHeap_InvSortedArray) {
   int *v = GetInverselySortedArray();
   Heap h(v, kArrayLength, Heap::HeapType::kMinHeap, &op_comp, &op_assign);
   EXPECT_TRUE(HeapTest::isHeap(h));
 }
+
+TEST_F(HeapTest, BuildHeap_TopDown_MaxHeap_AverageArray) {
+  int *v = GetAverageArray();
+  Heap h(Heap::HeapType::kMaxHeap, &op_comp, &op_assign);
+  for (int i = 0; i < kArrayLength; i++) {
+    h.push(v[i]);
+  }
+  EXPECT_TRUE(HeapTest::isHeap(h));
+}
+
+TEST_F(HeapTest, BuildHeap_TopDown_MaxHeap_SortedArray) {
+  int *v = GetSortedArray();
+  Heap h(Heap::HeapType::kMaxHeap, &op_comp, &op_assign);
+  for (int i = 0; i < kArrayLength; i++) {
+    h.push(v[i]);
+  }
+  EXPECT_TRUE(HeapTest::isHeap(h));
+}
+
+TEST_F(HeapTest, BuildHeap_TopDown_MaxHeap_InvSortedArray) {
+  int *v = GetInverselySortedArray();
+  Heap h(Heap::HeapType::kMaxHeap, &op_comp, &op_assign);
+  for (int i = 0; i < kArrayLength; i++) {
+    h.push(v[i]);
+  }
+  EXPECT_TRUE(HeapTest::isHeap(h));
+}
+
+TEST_F(HeapTest, BuildHeap_TopDown_MinHeap_AverageArray) {
+  int *v = GetAverageArray();
+  Heap h(Heap::HeapType::kMinHeap, &op_comp, &op_assign);
+  for (int i = 0; i < kArrayLength; i++) {
+    h.push(v[i]);
+  }
+  EXPECT_TRUE(HeapTest::isHeap(h));
+}
+
+TEST_F(HeapTest, BuildHeap_TopDown_MinHeap_SortedArray) {
+  int *v = GetSortedArray();
+  Heap h(Heap::HeapType::kMinHeap, &op_comp, &op_assign);
+  for (int i = 0; i < kArrayLength; i++) {
+    h.push(v[i]);
+  }
+  EXPECT_TRUE(HeapTest::isHeap(h));
+}
+
+TEST_F(HeapTest, BuildHeap_TopDown_MinHeap_InvSortedArray) {
+  int *v = GetInverselySortedArray();
+  Heap h(Heap::HeapType::kMinHeap, &op_comp, &op_assign);
+  for (int i = 0; i < kArrayLength; i++) {
+    h.push(v[i]);
+  }
+  EXPECT_TRUE(HeapTest::isHeap(h));
+}
+
