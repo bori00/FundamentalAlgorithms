c4c216922831858dd1e68aa44e8ceff87d135c36
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-10 13:09:25 +0300

    count operations when finding supposed root index

--- Lab1_Sorting/heap.cc
+++ Lab1_Sorting/heap.cc
@@ -24,19 +24,31 @@ int Heap::findSupposedRootIndex(int current_root_index) {
   int min_index, max_index;
   switch (heap_type) {
     case HeapType::kMinHeap:min_index = current_root_index;
-      if (min_index * 2 <= size && v[min_index] > v[min_index * 2]) {
-        min_index = min_index * 2;
+      if (min_index * 2 <= size) {
+        op_comp.count();
+        if (v[min_index] > v[min_index * 2]) {
+          min_index = min_index * 2;
+        }
       }
-      if (min_index * 2 + 1 <= size && v[min_index] > v[min_index * 2 + 1]) {
-        min_index = min_index * 2 + 1;
+      if (min_index * 2 + 1 <= size) {
+        op_comp.count();
+        if (v[min_index] > v[min_index * 2 + 1]) {
+          min_index = min_index * 2 + 1;
+        }
       }
       return min_index;
     case HeapType::kMaxHeap:max_index = current_root_index;
-      if (max_index * 2 <= size && v[max_index] < v[max_index * 2]) {
-        max_index = max_index * 2;
+      if (max_index * 2 <= size) {
+        op_comp.count();
+        if (v[max_index] < v[max_index * 2]) {
+          max_index = max_index * 2;
+        }
       }
-      if (max_index * 2 + 1 <= size && v[max_index] < v[max_index * 2 + 1]) {
-        max_index = max_index * 2 + 1;
+      if (max_index * 2 + 1 <= size) {
+        op_comp.count();
+        if (v[max_index] < v[max_index * 2 + 1]) {
+          max_index = max_index * 2 + 1;
+        }
       }
       return max_index;
   }
