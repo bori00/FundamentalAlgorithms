5db73c642cdad26f01b635f13a70b6e74cd8d121
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-10 12:41:11 +0300

    add google tests lib to git

---
+++ Lab1_Sorting/cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Bori/Documents/Egyetem/year2_sem1/University_Courses/Fundamental_Algorithms/lab/Repository/Lab1_Sorting/Google_tests/lib
+# Build directory: C:/Users/Bori/Documents/Egyetem/year2_sem1/University_Courses/Fundamental_Algorithms/lab/Repository/Lab1_Sorting/cmake-build-debug/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
