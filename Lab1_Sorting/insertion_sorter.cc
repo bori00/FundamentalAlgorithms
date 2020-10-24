bdd690470bff38752b9366d3cb57136a7aa5d418
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-24 14:42:59 +0300

    overload Sort in insertionsorter to pass operations as arguments

--- Lab1_Sorting/insertion_sorter.cc
+++ Lab1_Sorting/insertion_sorter.cc
@@ -12,16 +12,7 @@ const char *InsertionSorter::kSorterName = "InsertionSort";
 void InsertionSorter::Sort(int *v, int no_elements, Profiler &p) {
   Operation op_comp = p.createOperation(kCompOpName, no_elements);
   Operation op_assign = p.createOperation(kAssignOpName, no_elements);
-  int to_insert_value;
-  for (int i = 1; i < no_elements; i++) {
-    int insertion_index = getInsertionIndex(v, v[i], i, op_comp);
-    if (insertion_index != i) {
-      to_insert_value = v[i];
-      shiftElementsToRight(v, insertion_index, i - 1, op_assign);
-      v[insertion_index] = to_insert_value;
-      op_assign.count(2);
-    }
-  }
+  Sort(v, no_elements, &op_comp, &op_assign);
 }
 
 const char *InsertionSorter::GetCompOpName() {
@@ -37,26 +28,26 @@ const char *InsertionSorter::GetSorterName() {
 }
 
 int InsertionSorter::getInsertionIndex(const int *v, int value, int no_elements,
-                                       Operation &op_comp) {
+                                       Operation *op_comp) {
   int min = 0;
   int max = no_elements - 1;
   int middle;
   while (min < max) {
     middle = (min + max) / 2;
-    op_comp.count();
+    op_comp->count();
     if (v[middle] == value) {
       // ensure stability by inserting after the rightmost element equal to it
       int insertion_index = middle;
       while (insertion_index < no_elements && v[insertion_index] == value) {
         insertion_index++;
-        op_comp.count();
+        op_comp->count();
       }
       if (insertion_index < no_elements) {
-        op_comp.count();
+        op_comp->count();
       }
       return insertion_index;
     } else {
-      op_comp.count();
+      op_comp->count();
       if (v[middle] > value) {
         max = middle - 1;
       } else {
@@ -64,7 +55,7 @@ int InsertionSorter::getInsertionIndex(const int *v, int value, int no_elements,
       }
     }
   }
-  op_comp.count();
+  op_comp->count();
   if (v[min] > value) {
     return min;
   } else {
@@ -73,12 +64,25 @@ int InsertionSorter::getInsertionIndex(const int *v, int value, int no_elements,
 }
 
 void InsertionSorter::shiftElementsToRight(int *v, int low_index, int high_index,
-                                           Operation &op_assign) {
+                                           Operation *op_assign) {
   if (high_index >= low_index) {
     for (int i = high_index + 1; i >= low_index + 1; i--) {
       v[i] = v[i - 1];
     }
-    op_assign.count(high_index - low_index + 1);
+    op_assign->count(high_index - low_index + 1);
+  }
+}
+
+void InsertionSorter::Sort(int *v, int no_elements, Operation *op_comp, Operation *op_assign) {
+  int to_insert_value;
+  for (int i = 1; i < no_elements; i++) {
+    int insertion_index = getInsertionIndex(v, v[i], i, op_comp);
+    if (insertion_index != i) {
+      to_insert_value = v[i];
+      shiftElementsToRight(v, insertion_index, i - 1, op_assign);
+      v[insertion_index] = to_insert_value;
+      op_assign->count(2);
+    }
   }
 }
 
--- Lab1_Sorting/insertion_sorter.h
+++ Lab1_Sorting/insertion_sorter.h
@@ -14,6 +14,8 @@ class InsertionSorter : public Sorter {
  public:
   void Sort(int *v, int no_elements, Profiler &p) override;
 
+  void Sort(int *v, int no_elements, Operation* op_comp, Operation* op_assign);
+
   const char *GetCompOpName() override;
 
   const char *GetAssignOpName() override;
@@ -22,13 +24,13 @@ class InsertionSorter : public Sorter {
 
  private:
   /** Defines where the new element should be inserted in the already sorted part of the array. */
-  static int getInsertionIndex(const int *v, int value, int no_elements, Operation &op_comp);
+  static int getInsertionIndex(const int *v, int value, int no_elements, Operation *op_comp);
 
   /**
    * Shift the elements [low_index .. high_index] to the right by one position,
    * to [low_index + 1 .. high_index + 1]. The value of v[low_index] remains the same.
    */
-  static void shiftElementsToRight(int *v, int low_index, int high_index, Operation &op_assign);
+  static void shiftElementsToRight(int *v, int low_index, int high_index, Operation *op_assign);
 
   static const char *kAssignOpName;
   static const char *kCompOpName;
