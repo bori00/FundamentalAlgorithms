12f98c977887dca18ddb0c24d91ca12ba13774e3
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-27 10:28:53 +0200

    override comparison operators for Node

--- Lab4_kWayMerge/node.h
+++ Lab4_kWayMerge/node.h
@@ -11,19 +11,62 @@ class Node {
   Node* next;
 
  public:
-  explicit Node();
+  explicit Node(){
+    this->data = nullptr;
+    this->next = nullptr;
+  }
 
-  explicit Node(T data);
+  explicit Node(T data) {
+    this->data = data;
+    this->next = nullptr;
+  }
 
-  explicit Node(T data, Node* next);
+  explicit Node(T data, Node* next) {
+    // todo use the other constructor
+    this->data = data;
+    this->next = next;
+  }
 
-  void setNext(Node* node);
+  void setNext(Node* node) {
+    this->next = next;
+  }
 
-  T getData();
+  T getData() {
+    return this->data;
+  }
 
-  Node<T>* getNext();
+  Node<T>* getNext() {
+    return this->next;
+  }
 
-  bool hasNext();
+  bool hasNext() {
+    return this->next != nullptr;
+  }
+
+  friend bool operator >(const Node<T> &node1, const Node<T> &node2) {
+    return node1.data > node2.data;
+  }
+
+  friend bool operator <(const Node<T> &node1, const Node<T> &node2) {
+    return node1.data < node2.data;
+  }
+
+  friend bool operator >=(const Node<T> &node1, const Node<T> &node2) {
+    return node1.data >= node2.data;
+  }
+
+  friend bool operator <=(const Node<T> &node1, const Node<T> &node2) {
+    return node1.data <= node2.data;
+  }
+
+  friend bool operator ==(const Node<T> &node1, const Node<T> &node2) {
+    return node1.data == node2.data;
+  }
+
+  friend bool operator !=(const Node<T> &node1, const Node<T> &node2) {
+    return node1.data != node2.data;
+  }
+
 };
 
 #endif //LAB4_KWAYMERGE__NODE_H_
