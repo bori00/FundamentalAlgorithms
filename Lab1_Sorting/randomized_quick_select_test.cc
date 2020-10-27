b02543cd74512cb505090f7c61113da5408ea03f
Bori_Fazakas <fazakasbori@gmail.com>
2020-10-27 09:34:05 +0200

    fix typo

--- Lab1_Sorting/randomized_quick_select_test.cc
+++ Lab1_Sorting/randomized_quick_select_test.cc
@@ -10,7 +10,7 @@ using namespace std;
 
 int RandomizedQuickSelectTest::average_array_[] = {3, -7, 2, 10, -5, 9, 4, 12, 10, 10};
 int RandomizedQuickSelectTest::sorted_array_[] = {-10, 0, 3, 7, 10, 23, 101, 103, 1000, 1500};
-intRandomizedQuickSelectTest::inv_sorted_array_[] =
+int RandomizedQuickSelectTest::inv_sorted_array_[] =
     {1002, 987, 540, 10, -24, -105, -900, -901, -901, -1000};
 
 int *RandomizedQuickSelectTest::GetArrayWithContent(const int content[]) {
