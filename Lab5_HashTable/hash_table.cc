dac11cca67be7cea069f6c7dab707c5ce255f73c
Bori_Fazakas <fazakasbori@gmail.com>
2020-11-05 22:47:48 +0200

    implement methods for Entry type mapping

--- Lab5_HashTable/hash_table.cc
+++ Lab5_HashTable/hash_table.cc
@@ -4,51 +4,50 @@
 
 #include <iostream>
 #include "hash_table.h"
-#include "entry.h"
 
 using namespace std;
 
-template<class T, class Hasher>
-HashTable<T, Hasher>::HashTable() {
+template<class K, class V, class Hasher>
+HashTable<K, V, Hasher>::HashTable() {
   for (int i = 0; i <kSize; i++) {
     v[i] = nullptr;
   }
 }
 
-template<class T, class Hasher>
-bool HashTable<T, Hasher>::search(T* t) {
+template<class K, class V,  class Hasher>
+bool HashTable<K, V, Hasher>::search(K key) {
   bool found = false;
-  int hashcode = hasher_(*t);
+  int hashcode = hasher_(key);
   for (int i = 0; i < kSize && !found; i++) {
     int probe_index = hash(hashcode, i);
-    if (v[probe_index] != nullptr && *v[probe_index] == *t) {
+    if (v[probe_index] != nullptr && v[probe_index]->key == key) {
       found = true;
     }
   }
   return found;
 }
 
-template<class T, class Hasher>
-void HashTable<T, Hasher>::insert(T* t) {
+template<class K, class V, class Hasher>
+void HashTable<K, V, Hasher>::insert(K key, V value) {
   bool inserted = false;
-  int hashcode = hasher_(*t);
+  int hashcode = hasher_(key);
   for (int i = 0; i < kSize && !inserted; i++) {
     int probe_index = hash(hashcode, i);
-    if (v[probe_index] == nullptr) {
-      v[probe_index] = t;
+    if (v[probe_index] == nullptr || v[probe_index]->key == key) {
+      v[probe_index] = new Entry(key, value);
       inserted = true;
     }
   }
   if (!inserted) {
-    cerr << "Error: no empty slot left for element " << t << endl;
+    cerr << "Error: no empty slot left for element " << key << " " << value << endl;
     exit(1);
   }
 }
 
-template<class T, class Hasher>
-int HashTable<T, Hasher>::hash(int h, int i) {
+template<class K, class V, class Hasher>
+int HashTable<K, V, Hasher>::hash(int h, int i) {
   return (h + kC1*i + kC2 *i) % kSize;
 }
 
-template class HashTable<Entry, EntryHasher>;
+template class HashTable<int, string, hash<int>>;
 
