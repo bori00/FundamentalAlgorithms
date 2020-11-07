e0932cf194e84f266c8e1be87311fce38b89d53a
Bori_Fazakas <fazakasbori@gmail.com>
2020-11-07 14:16:01 +0200

    add tests for get function

--- Lab5_HashTable/hash_table_tests.cc
+++ Lab5_HashTable/hash_table_tests.cc
@@ -95,4 +95,66 @@ TEST(HashTable, SearchNotExistingStringIntElements) {
   }
 }
 
+TEST(HashTable, GetExistingIntStringElements) {
+  int no_elements = 9;
+  int keys[9] = {5, 101, 387, 1004, 298, 10012, -101, 10108};
+  string values[9] = {"five", "onehundredone", "threehundredeightyseven", "onethousandfour",
+                      "twohundredninetyeight", "tenthousandtwelve", "minusonehundredone",
+                      "tenthousandonehundredeight"};
+  HashTable<int, string, hash<int>> hash_table;
+  for (int i = 0; i < no_elements; i++) {
+    hash_table.insert(keys[i], values[i]);
+  }
+  for (int i = 0; i < no_elements; i++) {
+    EXPECT_EQ(values[i], *hash_table.get(keys[i]));
+  }
+}
+
+TEST(HashTable, GetExistingStringIntElements) {
+  int no_elements = 5;
+  int values[5] = {5, 101, 387, 1004, 298};
+  string keys[5] = {"five", "onehundredone", "threehundredeightyseven", "onethousandfour, "
+                                                                        "twohundredninetyeight"};
+  HashTable<string, int, hash<string>> hash_table;
+  for (int i = 0; i < no_elements; i++) {
+    hash_table.insert(keys[i], values[i]);
+  }
+  for (int i = 0; i < no_elements; i++) {
+    EXPECT_EQ(values[i], *hash_table.get(keys[i]));
+  }
+}
+
+TEST(HashTable, GetNotExistingIntStringElements) {
+  int no_elements = 9, no_not_elements = 4;
+  int keys[9] = {5, 101, 387, 1004, 298, 10012, -101, 10108};
+  int not_keys[4] = {7, -387, 105, 20019};
+  string values[9] = {"five", "onehundredone", "threehundredeightyseven", "onethousandfour",
+                      "twohundredninetyeight", "tenthousandtwelve", "minusonehundredone",
+                      "tenthousandonehundredeight"};
+  HashTable<int, string, hash<int>> hash_table;
+  for (int i = 0; i < no_elements; i++) {
+    hash_table.insert(keys[i], values[i]);
+  }
+  for (int i = 0; i < no_not_elements; i++) {
+    int no_probes = 0;
+    EXPECT_EQ(nullptr, hash_table.get(not_keys[i]));
+  }
+}
+
+TEST(HashTable, GetNotExistingStringIntElements) {
+  int no_elements = 5, no_not_elements = 5;
+  int values[5] = {5, 101, 387, 1004, 298};
+  string keys[5] = {"five", "onehundredone", "threehundredeightyseven", "onethousandfour, "
+                                                                        "twohundredninetyeight"};
+  string not_keys[5] = {"a", "aa", "abc", "abcd", "ada"};
+  HashTable<string, int, hash<string>> hash_table;
+  for (int i = 0; i < no_elements; i++) {
+    hash_table.insert(keys[i], values[i]);
+  }
+  for (int i = 0; i < no_not_elements; i++) {
+    int no_probes = 0;
+    EXPECT_EQ(nullptr, hash_table.get(not_keys[i]));
+  }
+}
+
 
