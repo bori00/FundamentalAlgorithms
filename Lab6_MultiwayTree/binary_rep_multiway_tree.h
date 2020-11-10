4a3a57d2baa5c9583a6d278910732f11eac6474a
Bori_Fazakas <fazakasbori@gmail.com>
2020-11-10 10:05:42 +0200

    represent multiway tree with binary nodes

---
+++ Lab6_MultiwayTree/binary_rep_multiway_tree.h
@@ -0,0 +1,34 @@
+//
+// Created by Bori on 11/10/2020.
+//
+
+#ifndef LAB6_MULTIWAYTREE__BINARY_REP_MULTIWAY_TREE_H_
+#define LAB6_MULTIWAYTREE__BINARY_REP_MULTIWAY_TREE_H_
+
+#include "multiway_tree.h"
+
+class BinaryRepMultiwayTree {
+ public:
+  explicit BinaryRepMultiwayTree(MultiwayTree m_tree);
+
+ private:
+  class BinaryNode {
+   public:
+    int data_;
+    BinaryNode* first_child_;
+    BinaryNode* first_sibling_;
+
+    BinaryNode(int data, BinaryNode* first_child, BinaryNode* first_sibling) {
+      this->data_ = data;
+      this->first_child_ = first_child;
+      this->first_sibling_ = first_sibling;
+    }
+  };
+
+  BinaryNode* getBinaryRepSubtree(MultiwayTree::MultiWayNode* multiway_root, BinaryNode*
+  first_sibling);
+
+  BinaryNode* root_;
+};
+
+#endif //LAB6_MULTIWAYTREE__BINARY_REP_MULTIWAY_TREE_H_
