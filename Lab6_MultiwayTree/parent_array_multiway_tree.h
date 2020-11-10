f35f034bb5f47b8dd7f40219e90edd772e154953
Bori_Fazakas <fazakasbori@gmail.com>
2020-11-10 10:03:59 +0200

    sketch representing the multiway tree with parent array

---
+++ Lab6_MultiwayTree/parent_array_multiway_tree.h
@@ -0,0 +1,19 @@
+//
+// Created by Bori on 11/10/2020.
+//
+
+#ifndef LAB6_MULTIWAYTREE__PARENT_ARRAY_MULTIWAY_TREE_H_
+#define LAB6_MULTIWAYTREE__PARENT_ARRAY_MULTIWAY_TREE_H_
+
+class ParentArrayMultiwayTree {
+ public:
+  ParentArrayMultiwayTree(int* parents, int no_nodes);
+
+ private:
+  int no_nodes_;
+  int* parents_;
+
+  friend class MultiwayTree;
+};
+
+#endif //LAB6_MULTIWAYTREE__PARENT_ARRAY_MULTIWAY_TREE_H_
