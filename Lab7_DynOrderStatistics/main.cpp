cdd650d559344fe241be4d4e1ff72947ccd70eb7
Bori_Fazakas <fazakasbori@gmail.com>
2020-11-22 14:45:26 +0200

    add chart interpretations

--- Lab7_DynOrderStatistics/main.cpp
+++ Lab7_DynOrderStatistics/main.cpp
@@ -52,6 +52,35 @@ void demo11();
  * Given that deletion in a BST is O(h), and here we originally build a PBT with h=lgn, and the
  * height can never be increased through deletion and selection, the complexity of this operation
  * is O(lgn).
+ *
+ * -- Chart interpretations
+ * To evaluate these operations in real life, trees of different n sizes between 100 and 10000 were
+ * built, and on each of them n selections and deletions were performed.
+ * - Build Tree
+ * The charts show clearly, than building a tree takes linear time (in my implementation, 2
+ * comparisons on data, 2 data assignments per node were performed).
+ * - Deletion
+ * Evaluating this chart is a little more complex, because a significant part of the operations
+ * are in fact performed on pointers (counted seperately and siplayed on separate charts).
+ * -> data assignments are performed on the deleted node, only if it has 2 children
+ * -> pointer assignments are performed on the actually deleted node only
+ * so these to operations are O(1) per deletion, so O(n) for n deletions.
+ * -> pointer comparisons are performed on the node to delete and on the path from it to its
+ * successor if it has children. So, this is in worst case O(lgn) per deletion, meaning O(nlgn)
+ * per n deletions, but in practice it is much smaller, since in most cases the successor is not
+ * needed or it is close to the node to delete
+ * -> data comparisons are performed once per node along the path from the root to the node to
+ * delete. This is worst case O(lgn), but the node to delete is a leaf only in ~half of the
+ * cases, so in practice the average complexity is a little better. For n deletions, this results
+ * in O(nlogn).
+ * All in all, we can see that data comparisons determine the overall complexity of a deletion,
+ * and what we can see on the chart corresponds to the expected O(nlogn) complexity (the charts
+ * shows in fact ~2nlogn operations for n deletions).
+ * - Selection
+ * The selection is an interesting case for evaluation, because it doesn't require any data
+ * assignments of comparisons: only sizes need to be computed(assigned) and compared. At each
+ * node along the search path at most 2 comparisons take place and 1 computation, resulting in
+ * ~2lgn operations, which corresponds to what we can see on the chart.
  */
 
 int main(int argc, char *argv[]) {
@@ -69,7 +98,9 @@ void demo11() {
   Operation op_assign_delete = p.createOperation("ignoreassigndelete", 0);
   Operation op_pointer_comp_delete = p.createOperation("ignorecompdeletepointer", 0);
   Operation op_pointer_assign_delete = p.createOperation("ignoreassigndeletepointer", 0);
-  OSTree os_tree(11);
+  Operation op_comp_build = p.createOperation("Build Comparisons", 0);
+  Operation op_assign_build = p.createOperation("Build Assignments", 0);
+  OSTree os_tree(11, &op_comp_build, &op_assign_build);
   cout << "------Original Tree" << endl;
   os_tree.PrettyPrint();
   cout << "------ Selected the 2nd element: " << os_tree.Select(2, &op_comp_select, &op_assign_select) << endl;
