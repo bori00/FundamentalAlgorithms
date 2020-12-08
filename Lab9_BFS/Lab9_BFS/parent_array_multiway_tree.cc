4f4687e0f968f0af0ce3d017b891a0a68d8e1685
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-08 13:09:41 +0200

    modify printing of parent array to 0 indexing

--- Lab9_BFS/Lab9_BFS/parent_array_multiway_tree.cc
+++ Lab9_BFS/Lab9_BFS/parent_array_multiway_tree.cc
@@ -19,9 +19,9 @@ ParentArrayMultiwayTree::ParentArrayMultiwayTree(int *parents, int no_nodes) {
 void ParentArrayMultiwayTree::Print() {
   for (int i = 0; i < no_nodes_; i++) {
     if (parents_[i] >= 0) {
-      cout << "Parent of node " << i + 1 << " is: " << parents_[i] + 1 << endl;
+      cout << "Parent of node " << i << " is: " << parents_[i] << endl;
     } else {
-      cout << "Node " << i + 1 << " is the root" << endl;
+      cout << "Node " << i << " is the root" << endl;
     }
   }
 }
