1bf2278fdca42a27b00b8a8ffe656876eb1d4c2b
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-21 17:00:24 +0200

    add demo for tarjan

---
+++ Lab10_DFS_TopS_Tarjan/demo_tarjan.h
@@ -0,0 +1,14 @@
+//
+// Created by Bori on 12/21/2020.
+//
+
+#ifndef LAB10_DFS_TOPS_TARJAN__DEMO_TARJAN_H_
+#define LAB10_DFS_TOPS_TARJAN__DEMO_TARJAN_H_
+
+void Demo_Tarjan1();
+
+void Demo_Tarjan2();
+
+void Demo_Tarjan3();
+
+#endif //LAB10_DFS_TOPS_TARJAN__DEMO_TARJAN_H_
